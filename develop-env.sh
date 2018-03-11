734402f56377b29fca2b8d667b935c2a9a7bbe5e
aliencoder <aiencoder@126.com>
2018-03-11 16:41:32 +0800

    docker add nginx and php-fpm

--- develop-env.sh
+++ develop-env.sh
@@ -18,8 +18,8 @@ mkdir /data/etc/nginx/conf.d -p
 mkdir /data/log/nginx -p
 
 curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/default.conf > /data/etc/nginx/conf.d/default.conf
-curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/index.html > /data/web/index.html
-curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/info.php > /data/web/info.php
+curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/index.html > /data/webroot/index.html
+curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/info.php > /data/webroot/info.php
 
 docker run --name my-php -d -v /data/webroot:/usr/share/nginx/html php:7.1-fpm
 docker run --name my-nginx -d -p 80:80 -v /data/webroot:/usr/share/nginx/html -v /data/etc/nginx/conf.d:/etc/nginx/conf.d -v /data/log/nginx:/var/log/nginx:rw --link my-php:php nginx
