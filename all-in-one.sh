4ccd53a82cd8d1c31dab64e84d7bf6e988d2e59f
aliencoder <aiencoder@126.com>
2018-03-11 14:53:20 +0800

    docker add nginx and php-fpm

--- all-in-one.sh
+++ all-in-one.sh
@@ -32,3 +32,11 @@ echo '{
 ' > /etc/docker/daemon.json
 
 service docker start
+
+mkdir /data/webroot -p
+mkdir /data/etc/nginx/conf.d -p
+mkdir /data/log -p
+
+curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/default.conf > mkdir /data/etc/nginx/conf.d/default.conf
+docker run --name my-php -d -v /data/webroot:/usr/share/nginx/html php:7.1-fpm
+docker run --name my-nginx -d -p 80:80 -v /data/webroot:/usr/share/nginx/html -v /data/etc/nginx/conf.d:/etc/nginx/conf.d -v /data/log:/var/log/nginx --link my-php:php nginx
