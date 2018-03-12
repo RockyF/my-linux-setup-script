64642d00a68680e66c2a882be1db167b3e0c6dec
aliencoder <aliencoder@126.com>
2018-03-12 11:43:27 +0800

    expose nginx.conf

--- all-in-one.sh
+++ all-in-one.sh
@@ -38,9 +38,10 @@ mkdir /data/webroot -p
 mkdir /data/etc/nginx/conf.d -p
 mkdir /data/log/nginx -p
 
+curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/nginx.conf > /data/etc/nginx/nginx.conf
 curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/default.conf > /data/etc/nginx/conf.d/default.conf
 #curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/index.html > /data/webroot/index.html
 #curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/info.php > /data/webroot/info.php
 
 docker run --name my-php -d --restart=always -v /data/webroot:/usr/share/nginx/html php:7.1-fpm
-docker run --name my-nginx -d --restart=always -p 80:80 -v /data/webroot:/usr/share/nginx/html -v /data/etc/nginx/conf.d:/etc/nginx/conf.d -v /data/log/nginx:/var/log/nginx:rw --link my-php:php nginx
+docker run --name my-nginx -d --restart=always -p 80:80 -v /data/webroot:/usr/share/nginx/html -v /data/etc/nginx/conf.d:/etc/nginx/conf.d -v /data/etc/nginx/nginx.conf:/etc/nginx/default.conf -v /data/log/nginx:/var/log/nginx --link my-php:php nginx
