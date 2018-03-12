2627ea2065e54a607391722e8dfd6eeee1fed1df
aliencoder <aliencoder@126.com>
2018-03-12 11:20:28 +0800

    add nodejs env

--- nodejs-env.sh
+++ nodejs-env.sh
@@ -3,4 +3,6 @@
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 source /root/.bashrc
 nvm install 8.9.4
+
+npm config set registry https://registry.npm.taobao.org
 npm i -g pm2
