bbd2bfd5115295e03ac983f7c39ef054add87357
AlienCoder <RockyF@users.noreply.github.com>
2018-05-11 11:04:24 +0800

    Update develop-env.sh

--- develop-env.sh
+++ develop-env.sh
@@ -20,8 +20,8 @@ mkdir -p /data/log/nginx
 
 curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/nginx.conf > /data/etc/nginx/nginx.conf
 curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/default.conf > /data/etc/nginx/conf.d/default.conf
-#curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/index.html > /data/webroot/index.html
-#curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/info.php > /data/webroot/info.php
+curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/index.html > /data/webroot/index.html
+curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/info.php > /data/webroot/info.php
 
 docker run --name my-php -d -u root --restart=always -v /data/webroot:/usr/share/nginx/html php:7.1-fpm
 docker run --name my-nginx -d --restart=always -p 80:80 -v /data/webroot:/usr/share/nginx/html -v /data/etc/nginx/conf.d:/etc/nginx/conf.d -v /data/etc/nginx/nginx.conf:/etc/nginx/nginx.conf -v /data/log/nginx:/var/log/nginx --link my-php:php nginx
