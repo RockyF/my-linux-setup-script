f77753cf8ce37d2db2b4e4939cd0717731d2233b
AlienCoder <RockyF@users.noreply.github.com>
2018-06-08 16:09:19 +0800

    add etcd
    add etcd

--- my-mac-dev-env.sh
+++ my-mac-dev-env.sh
@@ -69,6 +69,23 @@ install_nginx(){
 	--link my-php:php nginx
 }
 
+#------etcd
+install_etcd(){
+	HostIP='192.168.0.118'
+
+	docker run -d -v $userroot/etc/etcd/certs:/etc/ssl/certs -p 4001:4001 -p 2380:2380 -p 2379:2379 \
+	 --name etcd quay.io/coreos/etcd:v2.3.8\
+	 -name etcd0 \
+	 -advertise-client-urls http://${HostIP}:2379,http://${HostIP}:4001 \
+	 -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
+	 -initial-advertise-peer-urls http://${HostIP}:2380 \
+	 -listen-peer-urls http://0.0.0.0:2380 \
+	 -initial-cluster-token etcd-cluster-1 \
+	 -initial-cluster etcd0=http://${HostIP}:2380 \
+	 -initial-cluster-state new
+}
+
+
 if [ -n "$2" ]
 then
 	cmd=install_$2
@@ -79,4 +96,5 @@ else
 	install_redis
 	install_php
 	install_nginx
+	install_etcd
 fi
