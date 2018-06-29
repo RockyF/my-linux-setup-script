d4328d9363b07c136d46704a728269ad2df4b0cc
AlienCoder <RockyF@users.noreply.github.com>
2018-06-29 15:35:03 +0800

    Update develop-env.sh
    replace /data with $pwd

--- develop-env.sh
+++ develop-env.sh
@@ -14,14 +14,14 @@ echo '{
 
 service docker start
 
-mkdir -p /data/webroot
-mkdir -p /data/etc/nginx/conf.d
-mkdir -p /data/log/nginx
+mkdir -p $pwd/webroot
+mkdir -p $pwd/etc/nginx/conf.d
+mkdir -p $pwd/log/nginx
 
-curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/nginx.conf > /data/etc/nginx/nginx.conf
-curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/default.conf > /data/etc/nginx/conf.d/default.conf
-curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/index.html > /data/webroot/index.html
-curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/info.php > /data/webroot/info.php
+curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/nginx.conf > $pwd/etc/nginx/nginx.conf
+curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/default.conf > $pwd/etc/nginx/conf.d/default.conf
+curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/index.html > $pwd/webroot/index.html
+curl https://raw.githubusercontent.com/RockyF/my-linux-setup-script/master/nginx/info.php > $pwd/webroot/info.php
 
-docker run --name my-php -d -u root --restart=always -v /data/webroot:/usr/share/nginx/html php:7.1-fpm
-docker run --name my-nginx -d --restart=always -p 80:80 -v /data/webroot:/usr/share/nginx/html -v /data/etc/nginx/conf.d:/etc/nginx/conf.d -v /data/etc/nginx/nginx.conf:/etc/nginx/nginx.conf -v /data/log/nginx:/var/log/nginx --link my-php:php nginx
+docker run --name my-php -d -u root --restart=always -v $pwd/webroot:/usr/share/nginx/html php:7.1-fpm
+docker run --name my-nginx -d --restart=always -p 80:80 -v $pwd/webroot:/usr/share/nginx/html -v $pwd/etc/nginx/conf.d:/etc/nginx/conf.d -v $pwd/etc/nginx/nginx.conf:/etc/nginx/nginx.conf -v $pwd/log/nginx:/var/log/nginx --link my-php:php nginx
